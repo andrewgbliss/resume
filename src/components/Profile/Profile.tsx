9859cf6bcc9789d79d17b8175e25bfdbc1ceece9
Andrew Bliss <andrewgbliss@gmail.com>
2019-08-23 20:51:58 -0600

    updates

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -10,7 +10,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import profileImg from 'assets/img/andrew.png';
+import profileImg from 'assets/img/andrew2.png';
 import logoImg from 'assets/img/logo.png';
 
 const useStyles = makeStyles(theme => ({
